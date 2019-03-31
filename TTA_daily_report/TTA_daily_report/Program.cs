e80228bde1a07a1e958b9ebdd1acdd881bee1860
Matthew B <mjtb87@yahoo.com>
2019-03-30 21:30:18 -0600

    complete daily report drill

---
+++ TTA_daily_report/TTA_daily_report/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("The Tech Academy");
+        Console.WriteLine("Student Daily Report");
+        Console.ReadLine();
+        Console.WriteLine("What course are you on: ?");
+        string currentCourse = Console.ReadLine();
+        Console.WriteLine("What page number: ?");
+        string currentPageNumber = Console.ReadLine();
+        Console.WriteLine("Do you need help with anything? Please answer “true” or “false”. ?");
+        string helpNeed = Console.ReadLine();
+        Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+        string positiveExperiences = Console.ReadLine();
+        Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+        string otherFeedback = Console.ReadLine();
+        Console.WriteLine("How many hours did you study today?");
+        string hoursStudied = Console.ReadLine();
+        Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+        Console.ReadLine();
+    }
+}
