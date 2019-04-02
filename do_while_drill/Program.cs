b3172c809641977335c942f10948d8ec9a73cdaa
Matthew B <mjtb87@yahoo.com>
2019-04-02 12:12:44 -0600

    complete do while and while drills for C#

---
+++ do_while_drill/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace switchStatmentLesson
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("Hello number chooser! Time for some number fun!");
+            int user_running_total = 0;
+            string user_choice = "";
+            do
+            {
+                Console.WriteLine("Please choose a number that you want to multiply: ");
+                int user_base = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Now, choose a number to multiply by: ");
+                int user_multiple = Convert.ToInt32(Console.ReadLine());
+
+                user_running_total = user_running_total + user_base * user_multiple;
+                Console.WriteLine("Your running total is : " + user_running_total);
+
+                Console.WriteLine("Please press any key to continue or type 'quit' to exit program: ");
+                user_choice = Console.ReadLine();
+            }
+            while (user_choice != "quit");
+
+        }
+    }
+}
---
+++ while_loop_drill/while_loop_drill/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace switchStatmentLesson
+{
+    class Program
+    {
+        static void Main()
+        {
+            Random numberGenerator = new Random();
+            int number_of_guesses = 3;
+            int computer_number = numberGenerator.Next(1, 11);
+
+            Console.WriteLine("I wanna play a game...");
+            Console.WriteLine("You have three guesses at my secretly choosen number between one and ten.");
+            Console.WriteLine("Begin!");
+            while (number_of_guesses > 0)
+            {
+                Console.WriteLine("Guess number " + number_of_guesses + ":");
+                int user_guess = Convert.ToInt32(Console.ReadLine());
+                if (user_guess == computer_number)
+                {
+                    Console.WriteLine("You... how could you have defeated me. I must put more thought into my planning");
+                    Console.WriteLine("Press any key to walk away unphased");
+                    Console.ReadKey();
+                    return;
+                }
+                number_of_guesses = number_of_guesses - 1;       
+            }
+
+            Console.WriteLine("You have failed and now your life is mine. You came to the wrong carnival!");
+            Console.WriteLine("Press any key to forfeit your soul!");
+            Console.ReadKey();
+        }
+    }
+}
