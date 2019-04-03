45105036c9cd5d6f959965857221493bdb324327
Matthew B <mjtb87@yahoo.com>
2019-04-03 17:45:18 -0600

    complete large loop drill 1 for C#

---
+++ Interation/large_loop_drill_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] names = { "Shirley", "Bill", "Carl", "Chuck", "Leroy" };
+        string user_greeting = "";
+
+        do
+        {
+            Console.WriteLine("You don't you say hello to all my friends?");
+            user_greeting = Console.ReadLine();
+        }
+        while (user_greeting != "hello");
+
+        for (int i = 0; i < names.Length; i++)
+        {
+            Console.WriteLine(user_greeting + " " + names[i]);
+        }
+        Console.ReadKey();
+    }
+}
