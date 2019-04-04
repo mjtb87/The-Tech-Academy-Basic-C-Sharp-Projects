e91e60cf52528ea2784b216eab41e3891f537a9e
Matthew B <mjtb87@yahoo.com>
2019-04-04 17:24:39 -0600

    complete class_drill_2 for C#

---
+++ class method drill 2/Calculator_2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_method_drill_2
+{
+    class Calculator_2
+    {
+        public void  DoubleNum(int num_1, int num_2)
+        {
+            Console.WriteLine("Result of equation: " + num_1 * 2);
+            Console.WriteLine("Printed second number: " + num_2);
+
+        }
+    }
+}
---
+++ class method drill 2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_method_drill_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator_2 calculator_2 = new Calculator_2();
+            Console.WriteLine("Please select two whole numbers, one to double and the second to print.");
+    
+            try
+            {
+                int num_1 = Convert.ToInt32(Console.ReadLine());
+                int num_2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\n");
+                calculator_2.DoubleNum(num_1: num_1, num_2: num_2);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please type a whole number.");
+                return;
+            }
+      
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }            
+        }
+    }
+}
