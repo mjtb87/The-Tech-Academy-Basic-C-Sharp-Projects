39aa8bfa834f5ce8da4addb077356a6f39a91d11
Matthew B <mjtb87@yahoo.com>
2019-04-04 11:57:49 -0600

    complete class_drill_1 for C#

---
+++ class_drill_1/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_drill_1
+{
+    class Calculator
+    {
+        public static int DoubleNum(int num_1)
+        {
+            return (num_1 * 2);
+        }
+        public static double HalfNum(double num_1)
+        {
+            return (num_1 / 2);
+        }
+        public static int SquareNum(int num_1)
+        {
+            return (num_1 * num_1);
+        }
+    }
+}
---
+++ class_drill_1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_drill_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please select a function.");
+            Console.WriteLine("Please type 'D', 'H' or 'S'.");
+            string userChoice = Console.ReadLine();
+            userChoice = userChoice.ToUpper();
+
+
+            try
+            {
+                if (userChoice == "D")
+                {
+                    Console.WriteLine("Please select a number to double.");
+                    int num_1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(Calculator.DoubleNum(num_1));
+                }
+                else if (userChoice == "H")
+                {
+                    Console.WriteLine("Please select a number to cut in half.");
+                    int num_1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(Calculator.HalfNum(num_1));
+                }
+                else if (userChoice == "S")
+                {
+                    Console.WriteLine("Please select a number to square.");
+                    int num_1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(Calculator.SquareNum(num_1));
+                }
+                else
+                {
+                    Console.WriteLine("Please choose for the given options");
+                }
+                Console.ReadKey();
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Please type a whole number.");
+                    return;
+                }
+                catch (DivideByZeroException ex)
+                {
+                    Console.WriteLine("Please don't divide by zero.");
+                }
+
+
+            catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.ReadLine();
+                }
+        }
+    }
+}
