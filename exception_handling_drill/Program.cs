2d69eb707910af0e9b8ca83d7b808bc8bc369dd7
Matthew B <mjtb87@yahoo.com>
2019-04-03 20:40:15 -0600

    complete try catch drill C sharp

---
+++ exception_handling_drill/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> intList = new List<int>();
+        intList.Add(45);
+        intList.Add(36);
+        intList.Add(77);
+        intList.Add(98);
+        intList.Add(35);
+        intList.Add(10);
+        intList.Add(9);
+        intList.Add(68);
+        intList.Add(91);
+
+        Console.WriteLine("Please choose a number to divide by: ");
+
+        try
+        {
+            int divisor = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n");
+            foreach (int i in intList)
+            
+            {
+                Console.WriteLine(i / divisor);
+            }
+
+            Console.ReadKey();
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("Please type a whole number.");
+            return;
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Please don't divide by zero.");
+            return;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return;
+        }
+        finally
+        {
+            Console.ReadLine();
+        }
+        Console.WriteLine("Try//Catch block Finished - Continuing Program");
+        Console.ReadKey();
+    }
+}
