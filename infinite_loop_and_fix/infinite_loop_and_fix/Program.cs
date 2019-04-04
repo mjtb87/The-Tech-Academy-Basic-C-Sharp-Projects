6e18d4c5efd51d83d4ca4d8a348f373f3a388ce7
Matthew B <mjtb87@yahoo.com>
2019-04-03 18:26:15 -0600

    complete infinite loop drill with fix for C#

---
+++ infinite_loop_and_fix/infinite_loop_and_fix/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+//This part of the program has the infinite loop.
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] names = { "Jack", "Matt", "John", "Jacob", "Kermit", "Fozzy", "Gonzo", "Animal", "Sam"};
+        string greeting = "Oh... hello there, ";
+
+
+        for (int i = 0; i < names.Length;)
+        {
+            Console.WriteLine(greeting + names[i] + ".");
+        }
+
+        Console.ReadKey();
+    }
+}
+
+//The program below is the same as about, but with the fix for the infinite loop. 
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] names = { "Jack", "Matt", "John", "Jacob", "Kermit", "Fozzy", "Gonzo", "Animal", "Sam" };
+        string greeting = "Oh... hello there, ";
+
+
+        for (int i = 0; i < names.Length; i++)//<<<<<The fix is right here. We needed to increment out count by 1 each loop.
+        {
+            Console.WriteLine(greeting + names[i] + ".");
+        }
+
+        Console.ReadKey();
+    }
+}
+
