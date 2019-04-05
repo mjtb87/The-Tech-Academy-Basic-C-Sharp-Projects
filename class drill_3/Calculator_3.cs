d422fa2d8302e8a725798ed40b40428658af9a05
Matthew B <mjtb87@yahoo.com>
2019-04-04 20:56:06 -0600

    complete class_class_drill_3 for C #

---
+++ class drill_3/Calculator_3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_drill_3
+{
+    class Calculator_3
+    {
+        public void DoubleNum(int num_1)
+        {
+            Console.WriteLine("Result of equation: " + num_1 * 2);
+        }
+        public void Floor(double num_1)
+        {
+            Console.WriteLine("Result of equation: " + Math.Floor(num_1));
+        }
+        public void ConvertAndSquare(string num_1)
+        {
+            int num_2 = Convert.ToInt32(num_1);
+            Console.WriteLine("Result of equation: " + num_2 * num_2);
+        }
+    }
+}
---
+++ class drill_3/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_drill_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator_3 calculator_3 = new Calculator_3();
+
+            Console.WriteLine("Please select a function. Double, Floor, Or Square");
+            Console.WriteLine("Please type 'D', 'F' or 'S'.");
+            string userChoice = Console.ReadLine();
+            userChoice = userChoice.ToUpper();
+
+            try
+            {
+
+                if (userChoice == "D")
+                {
+                    Console.WriteLine("Please select a number to double.");
+                    int num_1 = Convert.ToInt32(Console.ReadLine());
+                    calculator_3.DoubleNum(num_1);
+                }
+                else if (userChoice == "F")
+                {
+                    Console.WriteLine("Please select a decimal to find the floor of.");
+                    double num_1 = Convert.ToDouble(Console.ReadLine());
+                    calculator_3.Floor(num_1);
+                }
+                else if (userChoice == "S")
+                {
+                    Console.WriteLine("Please select a number to square.");
+                    string num_1 = Console.ReadLine();
+                    calculator_3.ConvertAndSquare(num_1);
+                }
+                else
+                {
+                    Console.WriteLine("Please choose for the given options");
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please type a whole number.");
+                return;
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Please don't divide by zero.");
+            }
+
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
