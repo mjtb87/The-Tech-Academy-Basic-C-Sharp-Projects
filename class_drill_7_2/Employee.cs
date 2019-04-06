00ee55e617aea77729fdbbb611611fb932627b32
Matthew B <mjtb87@yahoo.com>
2019-04-06 16:37:16 -0600

    copy and rename class_drill_7 and update all files
    copy and rename class_drill_7 and remove parameters from interface file and reflect chang in rest of program

---
+++ class_drill_7_2/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_drill_7
+{
+    public class Employee : Person, IQuittable
+    {
+        public int Id { get; set; }
+        public bool IsWorking { get; set; }
+
+        public override void SayName()
+        {
+            base.SayName();
+        }
+        public void Quit()
+        {
+            Console.WriteLine("Would you like to quit?: ");
+            string userChoice = Console.ReadLine();
+            userChoice = userChoice.ToUpper();
+           do
+            {
+                if (userChoice == "Y")
+                {
+                    this.IsWorking = false;
+                }
+            }
+            while (userChoice != "Y" && userChoice != "N");
+        }   
+    }    
+}
---
+++ class_drill_7_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_drill_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Employee employee_1 = new Employee() { FirstName = "George", LastName = "Carlin", IsWorking = true };
+            IQuittable employee_2 = new Employee() { FirstName = "Mary", LastName = "Smith", IsWorking = true };
+
+            Console.WriteLine(employee_1.IsWorking);
+            employee_1.Quit();
+            Console.WriteLine(employee_1.IsWorking);
+
+           // Console.WriteLine(employee_2.IsWorking);
+            employee_2.Quit();
+           // Console.WriteLine(employee_2.IsWorking);
+
+            Console.ReadKey();
+        }
+    }
+}
