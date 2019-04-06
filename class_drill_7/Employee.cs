798a82f4b51c1596059082720a43747b01a0f56e
Matthew B <mjtb87@yahoo.com>
2019-04-05 18:29:30 -0600

    complete class_drill_7 for C#

---
+++ class_drill_7/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_drill_7
+{
+    class Employee : Person
+    {
+        public int Id { get; set; }
+
+        public override void SayName()
+        {
+            base.SayName();
+        }
+    }
+
+}
