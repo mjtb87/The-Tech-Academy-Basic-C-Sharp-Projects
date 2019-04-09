6663bde6e73916fb4f8dc5767bdc16f166be52eb
Matthew B <mjtb87@yahoo.com>
2019-04-08 20:01:21 -0600

    complete sctruct_drill for C#

---
+++ struct_drill/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace struct_drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num = new Number() {Amount = 1.61803m };
+            Console.WriteLine(num.Amount);
+            Console.ReadKey();
+        }
+
+        public struct Number
+        {
+            public decimal Amount { get; set; }
+        }
+    }
+}
