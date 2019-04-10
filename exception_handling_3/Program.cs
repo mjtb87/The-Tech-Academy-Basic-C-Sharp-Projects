17c71c9a53269851e79aa3041db3d66e892027a5
Matthew B <mjtb87@yahoo.com>
2019-04-10 10:36:22 -0600

    complete exception_handling_3 for C#

---
+++ exception_handling_3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exception_handling_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime todaysDate = DateTime.Now;
+            int currentYear = todaysDate.Year;
+                
+            try
+            {
+                Console.WriteLine("Please for the love of God enter your age!");
+                int age = Convert.ToInt32(Console.ReadLine());
+                if (age < 0)
+                {
+                    throw new ArgumentException();
+                }
+                int birthYear = currentYear - age;
+                Console.WriteLine(birthYear);
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("You can't be a negative age you loon!");
+                Console.ReadKey();
+                return;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something went wrong... somewhere...");
+                Console.ReadKey();
+                return;
+            }
+            Console.ReadKey();  
+        }
+    }
+}
