fecdf214a28215cc7bcb2d72516dac9b0678ccbe
Matthew B <mjtb87@yahoo.com>
2019-04-09 19:30:27 -0600

    complete date_time_drill for C#

---
+++ date_time_drill/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace date_time_drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+
+            Console.WriteLine("You need the time... in the future...");
+            Console.WriteLine("I can tell...");
+            Console.WriteLine("Give me a number...now.");
+
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(DateTime.Now.AddHours(userInput));
+
+            Console.ReadKey();
+        }
+    }
+}
